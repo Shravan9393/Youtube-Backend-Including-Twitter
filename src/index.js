033fc8bfa6194f3914605053bfb8bcb50af26eb2
Shravan Yadav <yadavshravan9393@gmail.com>
2024-12-05 22:59:55 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,63 @@
+// require('dotenv').config({path : './env'})
+
+import dotenv from "dotenv"
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path : './env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+import express from "express"
+const app = express()
+
+// function connectDB(){
+// }
+
+// using like above function 
+// use call type function
+// immediate function in javaScript i.e IIFEs
+// know as immediately invoked function expression
+// use semicolon before using iife's beacuse cleaning purpose
+// manlo phele line me koi colon nahi lagaya hoga 
+// then error aa skata haii nh bhaii
+// that's why
+
+;( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on("error",(error) => {
+            console.log("ERROR : ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("ERROR : ",error)
+        throw err
+    }
+})()
+
+*/
